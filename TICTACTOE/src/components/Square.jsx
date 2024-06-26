0799d7f6351948372d1bd87114b2ba35357ef06c
ANKITKUMAR1809 <skwedsak143@gmail.com>
2024-06-26 22:53:20 +0530

    css changed

--- TICTACTOE/src/components/Square.jsx
+++ TICTACTOE/src/components/Square.jsx
@@ -1,6 +1,6 @@
-// src/components/Square.jsx
+// its for square button giving the input as X or O
 import React from 'react';
-
+// value = X or O , color = blue or Red
 const Square = ({ value, onClick, color }) => {
   return (
     <button className={`square ${color}`} onClick={onClick}>
